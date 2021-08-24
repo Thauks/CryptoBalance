f37cbd2ba818151dee8d3edd4788ec8c0bfa016d
Jordi Mestres <jordimestrees@gmail.com>
2021-08-24 22:47:16 +0200

    Working
    Viable minimum product

--- src/CryptoWatcher.py
+++ src/CryptoWatcher.py
@@ -1,17 +1,23 @@
+import os
+import sys
+import time
+
 import yaml
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, run_async
 from coinbase.wallet.client import Client as CoinbaseClient
 from binance.client import Client as BinanceClient
+
+# Declaring config and the necessary clients to make the requests
 
 with open('../config/config.yml') as cnf:
     config = yaml.load(cnf, Loader=yaml.FullLoader)
 
-print(config)
-
 CbClient = CoinbaseClient(api_key=config['CB_API_KEY'], api_secret=config['CB_API_SECRET'])
 BnClient = BinanceClient(api_key=config['BN_API_KEY'], api_secret=config['CB_API_SECRET'])
 
+
+# Declaring the functions needed by the logic
 
 def get_balance(CbClient, BnClient):
     accounts = CbClient.get_accounts(limit=100)['data']
@@ -35,15 +41,28 @@ def get_balance(CbClient, BnClient):
             wallet_last += float(amount) * float(last_price)
             wallet_avg += float(amount) * float(avg_price)
 
-    return wallet_last, wallet_avg
+    return round(wallet_last, ndigits=2), round(wallet_avg, ndigits=2)
+
+
+def telegram_handler(update, context):
+    while True:
+        last, avg = get_balance(CbClient, BnClient)
+        update.message.reply_text(f'Current Balance: {last}')
+        print(context.args)
+        time.sleep(2)
+
+
+def stopper(update, context):
+    update.message.reply_text(f'Stopped.')
+    os._exit(0)
+
+
+# Running the bot
 
 updater = Updater(config['BOT_API_KEY'])
 
-dispatcher = updater.dispatcher
-
-# updater.dispatcher.add_handler(CommandHandler('hello', get_balance(CbClient, BnClient, updater)))
-
-
+updater.dispatcher.add_handler(CommandHandler('start', telegram_handler, run_async=True))
+updater.dispatcher.add_handler(CommandHandler('stop', stopper, run_async=True))
 
 updater.start_polling()
 updater.idle()
